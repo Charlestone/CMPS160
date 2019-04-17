b599c9896b7710cbea09b913d11a3d4efb688d65
Charlestone <carlosdr2332@gmail.com>
2019-04-16 23:02:37 -0700

    Minor changes

--- Base code/src/graphics/geometry/shapes/triangle.js
+++ Base code/src/graphics/geometry/shapes/triangle.js
@@ -12,17 +12,17 @@ class Triangle extends Geometry {
    * @param {Shader} shader Shading object used to shade geometry
    * @returns {Triangle} Triangle created
    */
-  constructor(shader, x, y, size) {
+  constructor(shader, x, y) {
       super(shader);
-      this.vertices = this.generateTriangleVertices(x, y, size);
+      this.vertices = this.generateTriangleVertices(x, y);
       this.faces = {0: this.vertices};
 
       // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
       this.interleaveVertices();
   }
 
-  generateTriangleVertices(x, y, size) {
-      var vertices = []
+  generateTriangleVertices(x, y) {
+      var vertices = [];
 
       var vertex1 = new Vertex((-0.05 * size + x), (-0.05 * size + y), 0.0);
       var vertex2 = new Vertex( (0.05 * size + x), (-0.05 * size + y), 0.0);
