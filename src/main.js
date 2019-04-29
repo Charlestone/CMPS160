ca5f4d00c71b7d873d489eec3d65acf656c09fd4
Charlestone <carlosdr2332@gmail.com>
2019-04-28 23:20:47 -0700

    Changed names, added the matrix declaration, added the cube

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var last_shape = 0;
 var TRIANGLE = 0;
 var SQUARE = 1;
 var CIRCLE = 2;
+var CUBE = 3;
 var red = 0.0;
 var green = 0.0;
 var blue = 0.0;
@@ -40,14 +41,20 @@ function main() {
   function set_circle() {
       last_shape = CIRCLE;
   }
+  document.getElementById("cube").onclick = function() {set_cube()};
+  function set_cube() {
+      last_shape = CUBE;
+  }
 
   // Initialize shader
-  shader = new Shader(gl, ASG1_VSHADER, ASG1_FSHADER);
+  shader = new Shader(gl, ASG2_VSHADER, ASG2_FSHADER);
 
   // Add attibutes
   shader.addAttribute("a_Position");
   shader.addAttribute("a_Color");
-
+  // Add Uniforms
+  var idMatrix = new Matrix4();
+  shader.addUniform("u_ModelMatrix", "mat4", idMatrix.elements);
   // Initialize renderer with scene and camera
   renderer = new Renderer(gl, scene, null);
   renderer.start();
