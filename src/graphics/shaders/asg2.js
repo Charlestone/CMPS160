59bbb0b1579a896210e2813517b76dc73eaabb45
Charlestone <carlosdr2332@gmail.com>
2019-04-28 23:21:38 -0700

    Rename of the shaders

--- src/graphics/shaders/asg2.js
+++ src/graphics/shaders/asg2.js
@@ -1,17 +1,17 @@
 // Vertex Shader
-var ASG1_VSHADER =
+var ASG2_VSHADER =
   `precision mediump float;
   attribute vec4 a_Position;
   attribute vec4 a_Color;
   varying vec4 v_Color;
-
+  uniform mat4 u_ModelMatrix;
   void main() {
     v_Color = a_Color;
-    gl_Position = a_Position;
+    gl_Position = u_ModelMatrix * a_Position;
   }`;
 
 // Fragment Shader
-var ASG1_FSHADER =
+var ASG2_FSHADER =
   `precision mediump float;
   varying vec4 v_Color;
 
