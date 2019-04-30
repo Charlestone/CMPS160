9175717a31ba6526ae59ac4bdcbc7d6a86593323
Charlestone <carlosdr2332@gmail.com>
2019-04-29 20:40:06 -0700

    Triangles for real this time

--- src/graphics/geometry/shapes/triangle.js
+++ src/graphics/geometry/shapes/triangle.js
@@ -18,17 +18,23 @@ class Triangle extends Geometry {
       this.faces = {0: this.vertices};
       this.cont = 1;
       this.scale = 1;
+      this.x = x;
+      this.y = y;
+      this.size = size;
       // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
       this.interleaveVertices();
   }
 
   generateTriangleVertices(x, y) {
       var vertices = [];
-
-      var vertex1 = new Vertex((-0.05 * size + x), (-0.05 * size + y), 0.0);
-      var vertex2 = new Vertex( (0.05 * size + x), (-0.05 * size + y), 0.0);
-      var vertex3 = new Vertex(  (0.0 * size + x),  (0.05 * size + y), 0.0);
-
+      var vertex1 = new Vertex(-0.05, -0.05, 0.0);
+      var vertex2 = new Vertex( 0.05, -0.05, 0.0);
+      var vertex3 = new Vertex( 0.0,   0.05, 0.0);
+      var init_position = new Matrix4();
+      var init_size = new Matrix4();
+      init_size.setScale(size, size, size);
+      init_position.setTranslate(x, y, 0);
+      this.shader.setUniform("u_ModelMatrix", init_position.multiply(init_size).elements);
       vertices.push(vertex1);
       vertices.push(vertex2);
       vertices.push(vertex3);
@@ -51,7 +57,13 @@ class Triangle extends Geometry {
       }
       this.cont--;
     }
-    this.ModelMatrix = aux;
-    this.shader.setUniform("u_ModelMatrix", this.ModelMatrix.elements);
+    var init_position = new Matrix4();
+    var init_size = new Matrix4();
+    var center = new Matrix4();
+    init_size.setScale(this.size, this.size, this.size);
+    init_position.setTranslate(this.x, this.y, 0);
+    center.setTranslate(0,0,0);
+    this.modelMatrix = init_position.multiply(aux.multiply(init_size.multiply(center)));
+    this.shader.setUniform("u_ModelMatrix", this.modelMatrix.elements);
   }
 }
