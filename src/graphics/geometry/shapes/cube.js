ec6530d9b028f5da72bc320ce0bf3e57078341d1
Charlestone <carlosdr2332@gmail.com>
2019-05-17 18:34:46 -0700

    Added rgb

--- src/graphics/geometry/shapes/cube.js
+++ src/graphics/geometry/shapes/cube.js
@@ -12,14 +12,18 @@ class Cube extends Geometry {
    * @param {Shader} shader Shading object used to shade geometry
    * @returns {Triangle} Triangle created
    */
-  constructor(shader, x, y, image, initial) {
+  constructor(shader, x, y, z, r, g, b, csize, image, initial) {
       super(shader);
       this.image = image;
       this.x = x;
       this.y = y;
-      this.size = size;
+      this.z = z;
+      this.r = r;
+      this.g = g;
+      this.b = b;
+      this.size = csize;
       this.aux = new Matrix4();
-      this.vertices = this.generateCubeVertices(x, y, initial);
+      this.vertices = this.generateCubeVertices(initial);
       this.faces = {0:[[this.vertices[0], this.vertices[1], this.vertices[2]],
                       [this.vertices[3], this.vertices[4], this.vertices[5]]],
                     1:[[this.vertices[6], this.vertices[7], this.vertices[8]],
@@ -37,17 +41,17 @@ class Cube extends Geometry {
       this.interleaveVertices();
   }
 
-  generateCubeVertices(x, y, initial) {
+  generateCubeVertices(initial) {
       var vertices = [];
       //vertices
-      var one =   [-0.05,  0.05,  0.05];
-      var two =   [ 0.05,  0.05,  0.05];
-      var three = [-0.05, -0.05,  0.05];
-      var four =  [ 0.05, -0.05,  0.05];
-      var five =  [-0.05,  0.05, -0.05];
-      var six =   [ 0.05,  0.05, -0.05];
-      var seven = [-0.05, -0.05, -0.05];
-      var eight = [ 0.05, -0.05, -0.05];
+      var one =   [-0.5, 0.5,  0.5];
+      var two =   [ 0.5, 0.5,  0.5];
+      var three = [-0.5, -0.5,  0.5];
+      var four =  [ 0.5, -0.5,  0.5];
+      var five =  [-0.5, 0.5, -0.5];
+      var six =   [ 0.5, 0.5, -0.5];
+      var seven = [-0.5, -0.5, -0.5];
+      var eight = [ 0.5, -0.5, -0.5];
       //uv coordinates
       var uv1 = [];
       var uv2 = [];
@@ -85,8 +89,6 @@ class Cube extends Geometry {
       var uv34 = [];
       var uv35 = [];
       var uv36 = [];
-      console.log('Image:');
-      console.log(this.image);
       if(this.image != null) {
         if(initial === undefined){
           //front face
@@ -177,53 +179,53 @@ class Cube extends Geometry {
         }
       }
       //front face 1
-      var vertex1 = new Vertex(one[0], one[1], one[2], uv1[0], uv1[1]);
-      var vertex2 = new Vertex(two[0], two[1], two[2], uv2[0], uv2[1]);
-      var vertex3 = new Vertex(three[0], three[1], three[2], uv3[0], uv3[1]);
+      var vertex1 = new Vertex(one[0], one[1], one[2], this.r, this.g, this.b, uv1[0], uv1[1]);
+      var vertex2 = new Vertex(two[0], two[1], two[2], this.r, this.g, this.b, uv2[0], uv2[1]);
+      var vertex3 = new Vertex(three[0], three[1], three[2], this.r, this.g, this.b, uv3[0], uv3[1]);
       //front face 2
-      var vertex4 = new Vertex(two[0], two[1], two[2], uv4[0], uv4[1]);
-      var vertex5 = new Vertex(three[0], three[1], three[2], uv5[0], uv5[1]);
-      var vertex6 = new Vertex(four[0], four[1], four[2], uv6[0], uv6[1]);
+      var vertex4 = new Vertex(two[0], two[1], two[2], this.r, this.g, this.b, uv4[0], uv4[1]);
+      var vertex5 = new Vertex(three[0], three[1], three[2], this.r, this.g, this.b, uv5[0], uv5[1]);
+      var vertex6 = new Vertex(four[0], four[1], four[2], this.r, this.g, this.b, uv6[0], uv6[1]);
       //back face 1
-      var vertex7 = new Vertex(five[0], five[1], five[2], uv7[0], uv7[1]);
-      var vertex8 = new Vertex(six[0], six[1], six[2], uv8[0], uv8[1]);
-      var vertex9 = new Vertex(seven[0], seven[1], seven[2], uv9[0], uv9[1]);
+      var vertex7 = new Vertex(five[0], five[1], five[2], this.r, this.g, this.b, uv7[0], uv7[1]);
+      var vertex8 = new Vertex(six[0], six[1], six[2], this.r, this.g, this.b, uv8[0], uv8[1]);
+      var vertex9 = new Vertex(seven[0], seven[1], seven[2], this.r, this.g, this.b, uv9[0], uv9[1]);
       //back face 2
-      var vertex10 = new Vertex(six[0], six[1], six[2], uv10[0], uv10[1]);
-      var vertex11 = new Vertex(seven[0], seven[1], seven[2], uv11[0], uv11[1]);
-      var vertex12 = new Vertex(eight[0], eight[1], eight[2], uv12[0], uv12[1]);
+      var vertex10 = new Vertex(six[0], six[1], six[2], this.r, this.g, this.b, uv10[0], uv10[1]);
+      var vertex11 = new Vertex(seven[0], seven[1], seven[2], this.r, this.g, this.b, uv11[0], uv11[1]);
+      var vertex12 = new Vertex(eight[0], eight[1], eight[2], this.r, this.g, this.b, uv12[0], uv12[1]);
       //top face 1
-      var vertex13 = new Vertex(five[0], five[1], five[2], uv13[0], uv13[1]);
-      var vertex14 = new Vertex(one[0], one[1], one[2], uv14[0], uv14[1]);
-      var vertex15 = new Vertex(two[0], two[1], two[2], uv15[0], uv15[1]);
+      var vertex13 = new Vertex(five[0], five[1], five[2], this.r, this.g, this.b, uv13[0], uv13[1]);
+      var vertex14 = new Vertex(one[0], one[1], one[2], this.r, this.g, this.b, uv14[0], uv14[1]);
+      var vertex15 = new Vertex(two[0], two[1], two[2], this.r, this.g, this.b, uv15[0], uv15[1]);
       //top face 2
-      var vertex16 = new Vertex(five[0], five[1], five[2], uv16[0], uv16[1]);
-      var vertex17 = new Vertex(six[0], six[1], six[2], uv17[0], uv17[1]);
-      var vertex18 = new Vertex(two[0], two[1], two[2], uv18[0], uv18[1]);
+      var vertex16 = new Vertex(five[0], five[1], five[2], this.r, this.g, this.b, uv16[0], uv16[1]);
+      var vertex17 = new Vertex(six[0], six[1], six[2], this.r, this.g, this.b, uv17[0], uv17[1]);
+      var vertex18 = new Vertex(two[0], two[1], two[2], this.r, this.g, this.b, uv18[0], uv18[1]);
       //bottom face 1
-      var vertex19 = new Vertex(seven[0], seven[1], seven[2], uv19[0], uv19[1]);
-      var vertex20 = new Vertex(three[0], three[1], three[2], uv20[0], uv20[1]);
-      var vertex21 = new Vertex(four[0], four[1], four[2], uv21[0], uv21[1]);
+      var vertex19 = new Vertex(seven[0], seven[1], seven[2], this.r, this.g, this.b, uv19[0], uv19[1]);
+      var vertex20 = new Vertex(three[0], three[1], three[2], this.r, this.g, this.b, uv20[0], uv20[1]);
+      var vertex21 = new Vertex(four[0], four[1], four[2], this.r, this.g, this.b, uv21[0], uv21[1]);
       //bottom face 2
-      var vertex22 = new Vertex(seven[0], seven[1], seven[2], uv22[0], uv22[1]);
-      var vertex23 = new Vertex(eight[0], eight[1], eight[2], uv23[0], uv23[1]);
-      var vertex24 = new Vertex(four[0], four[1], four[2], uv24[0], uv24[1]);
+      var vertex22 = new Vertex(seven[0], seven[1], seven[2], this.r, this.g, this.b, uv22[0], uv22[1]);
+      var vertex23 = new Vertex(eight[0], eight[1], eight[2], this.r, this.g, this.b, uv23[0], uv23[1]);
+      var vertex24 = new Vertex(four[0], four[1], four[2], this.r, this.g, this.b, uv24[0], uv24[1]);
       //right face 1
-      var vertex25 = new Vertex(two[0], two[1], two[2], uv25[0], uv25[1]);
-      var vertex26 = new Vertex(four[0], four[1], four[2], uv26[0], uv26[1]);
-      var vertex27 = new Vertex(eight[0], eight[1], eight[2], uv27[0], uv27[1]);
+      var vertex25 = new Vertex(two[0], two[1], two[2], this.r, this.g, this.b, uv25[0], uv25[1]);
+      var vertex26 = new Vertex(four[0], four[1], four[2], this.r, this.g, this.b, uv26[0], uv26[1]);
+      var vertex27 = new Vertex(eight[0], eight[1], eight[2], this.r, this.g, this.b, uv27[0], uv27[1]);
       //right face 2
-      var vertex28 = new Vertex(two[0], two[1], two[2], uv28[0], uv28[1]);
-      var vertex29 = new Vertex(six[0], six[1], six[2], uv29[0], uv29[1]);
-      var vertex30 = new Vertex(eight[0], eight[1], eight[2], uv30[0], uv30[1]);
+      var vertex28 = new Vertex(two[0], two[1], two[2], this.r, this.g, this.b, uv28[0], uv28[1]);
+      var vertex29 = new Vertex(six[0], six[1], six[2], this.r, this.g, this.b, uv29[0], uv29[1]);
+      var vertex30 = new Vertex(eight[0], eight[1], eight[2], this.r, this.g, this.b, uv30[0], uv30[1]);
       //left face 1
-      var vertex31 = new Vertex(one[0], one[1], one[2], uv31[0], uv31[1]);
-      var vertex32 = new Vertex(three[0], three[1], three[2], uv32[0], uv32[1]);
-      var vertex33 = new Vertex(seven[0], seven[1], seven[2], uv33[0], uv33[1]);
+      var vertex31 = new Vertex(one[0], one[1], one[2], this.r, this.g, this.b, uv31[0], uv31[1]);
+      var vertex32 = new Vertex(three[0], three[1], three[2], this.r, this.g, this.b, uv32[0], uv32[1]);
+      var vertex33 = new Vertex(seven[0], seven[1], seven[2], this.r, this.g, this.b, uv33[0], uv33[1]);
       //feft face 2
-      var vertex34 = new Vertex(one[0], one[1], one[2], uv34[0], uv34[1]);
-      var vertex35 = new Vertex(five[0], five[1], five[2], uv35[0], uv35[1]);
-      var vertex36 = new Vertex(seven[0], seven[1], seven[2], uv36[0], uv36[1]);
+      var vertex34 = new Vertex(one[0], one[1], one[2], this.r, this.g, this.b, uv34[0], uv34[1]);
+      var vertex35 = new Vertex(five[0], five[1], five[2], this.r, this.g, this.b, uv35[0], uv35[1]);
+      var vertex36 = new Vertex(seven[0], seven[1], seven[2], this.r, this.g, this.b, uv36[0], uv36[1]);
 
       vertices.push(vertex1);
       vertices.push(vertex2);
@@ -264,26 +266,12 @@ class Cube extends Geometry {
 
       var init_position = new Matrix4();
       var init_size = new Matrix4();
-      init_size.setScale(size, size, size);
-      init_position.setTranslate(x, y, 0);
-      var init_tilting = new Matrix4();
-      init_tilting.setRotate(20,0,0,1);
+      init_size.setScale(this.size, this.size, this.size);
+      init_position.setTranslate(this.x, this.y, this.z);
       vertices.forEach(function(it){
-        it.point = init_tilting.multiplyVector3(init_size.multiplyVector3(it.point));
+        it.point = init_size.multiplyVector3(it.point);
       });
       this.shader.setUniform("u_ModelMatrix", init_position.elements);
-      console.log(vertices);
       return vertices;
-  }
-  render() {
-    var aux = new Matrix4();
-    aux.setRotate(1,0,1,0);
-    this.aux = aux.multiply(this.aux);
-    var init_position = new Matrix4();
-    var center = new Matrix4();
-    init_position.setTranslate(this.x, this.y, 0);
-    center.setTranslate(0,0,0);
-    this.modelMatrix = init_position.multiply(this.aux.multiply(center));
-    this.shader.setUniform("u_ModelMatrix", this.modelMatrix.elements);
   }
 }
