830de08824aa2684fa870f4cd52d8f99812a016e
Charlestone <carlosdr2332@gmail.com>
2019-05-20 21:17:26 -0700

    Add class light

---
+++ src/graphics/light.js
@@ -0,0 +1,10 @@
+
+class Light {
+    constructor(x, y, z) {
+        this.pos = new Vector3([x, y, z]);
+        //Light colors
+        this.ambient = [0.5, 0.5, 0.5];
+        this.diffuse = [0.5, 0.5, 0.5];
+        this.specular = [1.0, 1.0, 1.0];
+    }
+}
