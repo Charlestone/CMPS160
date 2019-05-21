771f5fe9ac33299e277bc6c9c62b698394eb29db
Charlestone <carlosdr2332@gmail.com>
2019-05-20 19:48:39 -0700

    Added normal and setNormal to vertex

--- src/graphics/geometry/vertex.js
+++ src/graphics/geometry/vertex.js
@@ -9,10 +9,14 @@ class Vertex {
     this.point  = new Vector3([x, y, z]);
   	this.color  = [r, g, b, 1.0];
   	this.textCoord = [u, v];
+  	this.normal = new Vector3();
 
       
 
       // This class can be extended to support other attributes such as
       // normals and UV coordinates.
+  }
+  setNormal(x, y, z){
+  	this.normal.elements = [x, y, z];
   }
 }
