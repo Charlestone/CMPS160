f08060ad6e1bbdc9505171b8a16a4c98fd045849
Charlestone <carlosdr2332@gmail.com>
2019-05-25 12:29:43 -0700

    Reduced ambient light

--- src/graphics/light.js
+++ src/graphics/light.js
@@ -3,7 +3,7 @@ class Light {
     constructor(x, y, z) {
         this.pos = new Vector3([x, y, z]);
         //Light colors
-        this.ambient = [0.5, 0.5, 0.5];
+        this.ambient = [0.3, 0.3, 0.3];
         this.diffuse = [0.5, 0.5, 0.5];
         this.specular = [1.0, 1.0, 1.0];
     }
