3839ff8e64c98eb78f80fa119de63dec7ed25798
Charlestone <carlosdr2332@gmail.com>
2019-05-25 18:40:27 -0700

    Changing different light values

--- src/graphics/light.js
+++ src/graphics/light.js
@@ -3,8 +3,8 @@ class Light {
     constructor(x, y, z) {
         this.pos = new Vector3([x, y, z]);
         //Light colors
-        this.ambient = [0.3, 0.3, 0.3];
-        this.diffuse = [0.5, 0.5, 0.5];
+        this.ambient = [1.0, 1.0, 1.0];
+        this.diffuse = [1.0, 1.0, 1.0];
         this.specular = [1.0, 1.0, 1.0];
     }
 }
